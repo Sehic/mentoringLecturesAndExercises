98eeacfd7d4abbdf50be59f5a415efa9789eaf92
Hajrudin Sehic <hajrudin.sehic@BitCamps-Mac-mini-9.local>
2015-07-15 12:25:09 +0200

    Initial commit

--- MiniGame/src/ba/bitcamp/hajrudin/mini/game/Game.java
+++ MiniGame/src/ba/bitcamp/hajrudin/mini/game/Game.java
@@ -36,7 +36,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.Timer;
 
-public class Game extends JFrame{
+public class Game extends JFrame {
 	private static final long serialVersionUID = 694570934410006456L;
 
 	private JPanel mainPanel = new JPanel();
@@ -56,46 +56,47 @@ public class Game extends JFrame{
 	private static JLabel scoreLabel = new JLabel("Score: 0");
 	private JLabel levelLabel = new JLabel("Level: 1");
 	private JButton showScoresButton = new JButton("Show scores");
-	
+
 	private int enemySpeed = 5;
 	private int mySpeed = 5;
 	private int bulletSpeed = 5;
-	
+
 	private MyPanel game = new MyPanel();
 
 	private JLabel playLabel = new JLabel();
-	
+
 	private Timer t = new Timer(400, new AnimatedTank());
 	private Timer t1 = new Timer(10, new Shot());
 	private Timer t2 = new Timer(50, new EnemyShot());
-	
-	private Rectangle myTank = new Rectangle(800/2, 600-115, 60, 60);
-	private Rectangle newTank = new Rectangle(800/2, -70, 60, 60);
-	
-	private Rectangle myBullet = new Rectangle(myTank.x+23, myTank.y,15,15);
+
+	private Rectangle myTank = new Rectangle(800 / 2, 600 - 115, 60, 60);
+	private Rectangle newTank = new Rectangle(800 / 2, -70, 60, 60);
+
+	private Rectangle myBullet = new Rectangle(myTank.x + 23, myTank.y, 15, 15);
 	private boolean temp = false;
-	
-	private Rectangle newBullet = new Rectangle(newTank.x+23, newTank.y+55,15,15);
+
+	private Rectangle newBullet = new Rectangle(newTank.x + 23, newTank.y + 55,
+			15, 15);
 	private boolean temp1 = false;
-	
-	private Rectangle speed = new Rectangle((int)(Math.random()*400),-300,20,21);
-	private Timer ts = new Timer (300, new AnimatedSpeed());
-	
+
+	private Rectangle speed = new Rectangle((int) (Math.random() * 400), -300,
+			20, 21);
+	private Timer ts = new Timer(300, new AnimatedSpeed());
+
 	private Font f = new Font("SERIF", Font.BOLD, 20);
-	private Color c = new Color(134,128,118);
-	private Color c1 = Color.GRAY; 
+	private Color c = new Color(134, 128, 118);
+	private Color c1 = Color.GRAY;
 	private Color c2 = Color.GRAY;
-	
+
 	private Font f1 = new Font("SERIF", Font.ITALIC, 17);
-	
+
 	private BufferedImage background;
 	private BufferedImage myPlane;
 	private BufferedImage myRocket;
 	private BufferedImage newPlane;
 	private BufferedImage newRocket;
 	private BufferedImage topSpeed;
-	
-	
+
 	public Game() {
 
 		mainPanel.setLayout(new BorderLayout());
@@ -133,17 +134,20 @@ public class Game extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					Desktop.getDesktop().browse(new URL("https://github.com/Sehic/mentoringLecturesAndExercises/tree/master/MiniGame/src/ba/bitcamp/hajrudin/mini/game").toURI());
+					Desktop.getDesktop()
+							.browse(new URL(
+									"https://github.com/Sehic/mentoringLecturesAndExercises/tree/master/MiniGame/src/ba/bitcamp/hajrudin/mini/game")
+									.toURI());
 				} catch (IOException | URISyntaxException e1) {
 					e1.printStackTrace();
 				}
 			}
 		});
-		
+
 		mainPanel.add(gamePanel, BorderLayout.CENTER);
-		
+
 		gamePanel.setLayout(new BorderLayout());
-		
+
 		gamePanel.setVisible(false);
 		gamePanel.add(infoPanel, BorderLayout.NORTH);
 		infoPanel.setLayout(new GridLayout(1, 4));
@@ -161,10 +165,9 @@ public class Game extends JFrame{
 		showScoresButton.setBackground(c2);
 		showScoresButton.setForeground(Color.BLACK);
 		showScoresButton.setOpaque(true);
-		
+
 		showScoresButton.addActionListener(new ShowScoresButton());
-		
-		
+
 		gamePanel.add(game, BorderLayout.CENTER);
 		Font f = new Font("SERIF", Font.CENTER_BASELINE, 33);
 		playLabel.setFont(f);
@@ -174,21 +177,28 @@ public class Game extends JFrame{
 		game.addKeyListener(new MyTankControl());
 		game.addMouseListener(new MyTankMouse());
 		game.addFocusListener(new MyTankFocus());
-		
-		 try {                
-	         
-			 background = ImageIO.read(new URL("http://www.axsysnav.eu/wp-content/uploads/2014/07/blue-space-wallpaper-8.png"));
-			 myPlane = ImageIO.read(new URL("http://s18.postimg.org/hcj11q0t1/1436280574_kspaceduel.png"));
-			 myRocket = ImageIO.read(new URL("http://s18.postimg.org/j7shr12fp/1436281130_rocket.png"));
-			 newPlane = ImageIO.read(new URL("http://s29.postimg.org/tadxmuldf/clipart_wolf_space_ship_256x256_b434.png"));
-			 newRocket = ImageIO.read(new URL("http://s2.postimg.org/m8uzzql45/rocket.png"));
-			 topSpeed = ImageIO.read(new URL("http://s14.postimg.org/q1ezs0359/1094748.png"));
-		 } catch (IOException ex) {
-	            System.out.println("aaaaaaa");
-	       }
-		 
-		 
-		
+
+		try {
+
+			background = ImageIO
+					.read(new URL(
+							"http://www.axsysnav.eu/wp-content/uploads/2014/07/blue-space-wallpaper-8.png"));
+			myPlane = ImageIO
+					.read(new URL(
+							"http://s18.postimg.org/hcj11q0t1/1436280574_kspaceduel.png"));
+			myRocket = ImageIO.read(new URL(
+					"http://s18.postimg.org/j7shr12fp/1436281130_rocket.png"));
+			newPlane = ImageIO
+					.read(new URL(
+							"http://s29.postimg.org/tadxmuldf/clipart_wolf_space_ship_256x256_b434.png"));
+			newRocket = ImageIO.read(new URL(
+					"http://s2.postimg.org/m8uzzql45/rocket.png"));
+			topSpeed = ImageIO.read(new URL(
+					"http://s14.postimg.org/q1ezs0359/1094748.png"));
+		} catch (IOException ex) {
+			System.out.println("aaaaaaa");
+		}
+
 		setTitle("Mini Game");
 		setSize(1000, 600);
 		setResizable(false);
@@ -196,304 +206,279 @@ public class Game extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
-	private class AnimatedTank implements ActionListener{
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		newTank.y = newTank.y+enemySpeed;
-		if (newTank.getY() > 485) {
-			newTank.y = 0;
-			newTank.x = ((int) (Math.random() * 650));
-			String arr[] = lifesLabel.getText().split(" ");
-			int a = Integer.parseInt(arr[1]);
-			a-=1;
-			lifesLabel.setText(arr[0]+" "+a);
-			if(a==0){
-			gameOver();
+
+	private class AnimatedTank implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			newTank.y = newTank.y + enemySpeed;
+			if (newTank.getY() > 485) {
+				newTank.y = 0;
+				newTank.x = ((int) (Math.random() * 650));
+				String arr[] = lifesLabel.getText().split(" ");
+				int a = Integer.parseInt(arr[1]);
+				a -= 1;
+				lifesLabel.setText(arr[0] + " " + a);
+				if (a == 0) {
+					gameOver();
+				}
+			}
+			if (newTank.getY() > 0) {
+				t2.start();
+			}
+			game.repaint();
 		}
-		}	
-		if(newTank.getY()>0){
-		t2.start();
-		}
-		game.repaint();
 	}
-	}
-	
+
 	private class MyPanel extends JPanel {
 		private static final long serialVersionUID = 193885030828890505L;
 
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
-			g.drawImage(background, game.getX()-30, game.getY()-30, null);
-			
-			g.drawImage(myPlane, myTank.x, myTank.y,null);
-			
-			g.drawImage(newPlane, newTank.x, newTank.y,null);
-			
-			if(temp)
-				
+
+			g.drawImage(background, game.getX() - 30, game.getY() - 30, null);
+
+			g.drawImage(myPlane, myTank.x, myTank.y, null);
+
+			g.drawImage(newPlane, newTank.x, newTank.y, null);
+
+			if (temp)
+
 				g.drawImage(myRocket, myBullet.x, myBullet.y, null);
 			else
-			
-				g.drawImage(myRocket, myTank.x+23, myTank.y, null);
-			
-			if(temp1)
-			
+
+				g.drawImage(myRocket, myTank.x + 23, myTank.y, null);
+
+			if (temp1)
+
 				g.drawImage(newRocket, newBullet.x, newBullet.y, null);
 			else
-			
-				g.drawImage(newRocket, newTank.x+23, newTank.y+55,null);
-			
+
+				g.drawImage(newRocket, newTank.x + 23, newTank.y + 55, null);
+
 			g.drawImage(topSpeed, speed.x, speed.y, null);
-			
+
 		}
-	} 
-	
-	private class AnimatedSpeed implements ActionListener{
+	}
+
+	private class AnimatedSpeed implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			speed.y+=5;
-			if(speed.intersects(myTank)){
-				mySpeed+=5;
-				speed.y=-700;
+			speed.y += 5;
+			if (speed.intersects(myTank)) {
+				mySpeed += 5;
+				speed.y = -700;
 			}
-			if(speed.y>485){
-				speed.y=-700;
+			if (speed.y > 485) {
+				speed.y = -700;
 			}
 			game.repaint();
 		}
-		
+
 	}
-	
-	private class MyTankControl extends KeyAdapter{
-	
+
+	private class MyTankControl extends KeyAdapter {
+
 		@Override
 		public void keyPressed(KeyEvent e) {
 			super.keyPressed(e);
-			if(e.getKeyCode() == KeyEvent.VK_LEFT){
-				myTank.x-=mySpeed;
-				if(myTank.x<=0){
-					myTank.x+=mySpeed;
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				myTank.x -= mySpeed;
+				if (myTank.x <= 0) {
+					myTank.x += mySpeed;
 				}
-			}else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-				myTank.x+=mySpeed;
-				if(myTank.x>=670){
-					myTank.x-=mySpeed;
+			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				myTank.x += mySpeed;
+				if (myTank.x >= 670) {
+					myTank.x -= mySpeed;
 				}
-			}else if(e.getKeyCode() == KeyEvent.VK_SPACE){
-				myBullet.x=myTank.x+23;				
+			} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				myBullet.x = myTank.x + 23;
 				t1.start();
 			}
 			game.repaint();
 		}
-		
+
 	}
-	
-	private int counter=0;
-	
-	private class Shot implements ActionListener{
+
+	private int counter = 0;
+
+	private class Shot implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			temp=true;
-			myBullet.y-=5;
-			if(myBullet.intersects(newTank)){
+			temp = true;
+			myBullet.y -= 5;
+			if (myBullet.intersects(newTank)) {
 				String arr[] = scoreLabel.getText().split(" ");
 				int a = Integer.parseInt(arr[1]);
-				a+=5;
-				scoreLabel.setText(arr[0]+" "+a);
-				counter+=1;
-				if(counter == 5){
+				a += 5;
+				scoreLabel.setText(arr[0] + " " + a);
+				counter += 1;
+				if (counter == 5) {
 					counter = 0;
 					String ar[] = levelLabel.getText().split(" ");
 					int b = Integer.parseInt(ar[1]);
-					b+=1;
-					levelLabel.setText(ar[0]+" "+b);
+					b += 1;
+					levelLabel.setText(ar[0] + " " + b);
 					enemySpeed += 5;
-					bulletSpeed+=1;
+					bulletSpeed += 1;
 				}
 				t2.stop();
-				temp1=false;
+				temp1 = false;
 				newTank.y = 0;
 				newTank.x = ((int) (Math.random() * 730));
-				myBullet.y=myTank.y;
-				newBullet.x=newTank.x+23;
-				newBullet.y=newTank.y+55;
-				temp=false;
+				myBullet.y = myTank.y;
+				newBullet.x = newTank.x + 23;
+				newBullet.y = newTank.y + 55;
+				temp = false;
 				t1.stop();
 			}
-			if(myBullet.y < 0){
-				myBullet.y=myTank.y;
-				temp=false;
+			if (myBullet.y < 0) {
+				myBullet.y = myTank.y;
+				temp = false;
 				t1.stop();
 			}
 			game.repaint();
 		}
-		}
-	
-	public void gameOver(){
+	}
+
+	public void gameOver() {
 		writeScore();
 		ts.stop();
 		t.stop();
 		t1.stop();
-		t2.stop();	
-		int tt = JOptionPane.showConfirmDialog(null, "GAME OVER \n "+scoreLabel.getText()+ "\n REPLAY?");
-		if(tt == JOptionPane.YES_OPTION){
+		t2.stop();
+		int tt = JOptionPane.showConfirmDialog(null, "GAME OVER \n "
+				+ scoreLabel.getText() + "\n REPLAY?");
+		if (tt == JOptionPane.YES_OPTION) {
 			lifesLabel.setText("Lifes: 3");
 			scoreLabel.setText("Score: 0");
 			levelLabel.setText("Level: 1");
 			enemySpeed = 5;
 			mySpeed = 5;
-			bulletSpeed=5;
-			myTank.x=800/2; myTank.y=600-115;
-			newTank.x=800/2; newTank.y=-70;
-			myBullet.x=myTank.x+23; myBullet.y=myTank.y;
+			bulletSpeed = 5;
+			myTank.x = 800 / 2;
+			myTank.y = 600 - 115;
+			newTank.x = 800 / 2;
+			newTank.y = -70;
+			myBullet.x = myTank.x + 23;
+			myBullet.y = myTank.y;
 			temp = false;
-			newBullet = new Rectangle(newTank.x+23, newTank.y+55,15,15);
+			newBullet = new Rectangle(newTank.x + 23, newTank.y + 55, 15, 15);
 			temp1 = false;
-			speed.x = ((int)(Math.random()*400)); speed.y=-300;
+			speed.x = ((int) (Math.random() * 400));
+			speed.y = -300;
 			playLabel.setText("CLICK TO PLAY");
 			game.addKeyListener(new MyTankControl());
 			game.addMouseListener(new MyTankMouse());
 			game.addFocusListener(new MyTankFocus());
-		}else if(tt == JOptionPane.CANCEL_OPTION || tt == JOptionPane.NO_OPTION || tt == JOptionPane.CLOSED_OPTION){
+		} else if (tt == JOptionPane.CANCEL_OPTION
+				|| tt == JOptionPane.NO_OPTION
+				|| tt == JOptionPane.CLOSED_OPTION) {
 			game.setVisible(false);
 		}
 	}
-	
-	private class EnemyShot implements ActionListener{
+
+	private class EnemyShot implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			temp1=true;
-			newBullet.y+=bulletSpeed;
-			if(newBullet.intersects(myTank)){
+			temp1 = true;
+			newBullet.y += bulletSpeed;
+			if (newBullet.intersects(myTank)) {
 				String arr[] = lifesLabel.getText().split(" ");
 				int a = Integer.parseInt(arr[1]);
-				a-=1;
-				lifesLabel.setText(arr[0]+" "+a);
-				if(a==0){
-				gameOver();
+				a -= 1;
+				lifesLabel.setText(arr[0] + " " + a);
+				if (a == 0) {
+					gameOver();
 				} else {
-					newBullet.x=newTank.x+23;
-					newBullet.y=newTank.y+55;
-					temp1=false;
+					newBullet.x = newTank.x + 23;
+					newBullet.y = newTank.y + 55;
+					temp1 = false;
 					game.repaint();
 				}
 			}
-			if(newBullet.y > 550){
-				newBullet.x=newTank.x+23;
-				newBullet.y=newTank.y+55;
-				temp1=false;
+			if (newBullet.y > 550) {
+				newBullet.x = newTank.x + 23;
+				newBullet.y = newTank.y + 55;
+				temp1 = false;
 			}
 			game.repaint();
 		}
+	}
+
+	private class MyTankMouse extends MouseAdapter {
+		public void mousePressed(MouseEvent evt) {
+			playLabel.setText("");
+			t.start();
+			ts.start();
+			game.requestFocus();
 		}
-	
-	private class MyTankMouse extends MouseAdapter{
-			public void mousePressed(MouseEvent evt) {
-				playLabel.setText("");
-				t.start();
-				ts.start();
-				game.requestFocus();
-			}
-		}
-	
-	private class MyTankFocus extends FocusAdapter{
+	}
+
+	private class MyTankFocus extends FocusAdapter {
 		public void focusGained(FocusEvent evt) {
 			game.repaint();
 		}
+
 		public void focusLost(FocusEvent evt) {
 			game.repaint();
 		}
 	}
 
-	private class TextAction implements KeyListener{
+	private class TextAction implements KeyListener {
 		@Override
 		public void keyTyped(KeyEvent e) {
 			startGameButton.setEnabled(true);
 		}
+
 		@Override
-		public void keyPressed(KeyEvent e) {}
+		public void keyPressed(KeyEvent e) {
+		}
+
 		@Override
-		public void keyReleased(KeyEvent e) {}
+		public void keyReleased(KeyEvent e) {
+		}
 	}
-	
-	public static void writeScore(){
+
+	public static void writeScore() {
 		TextIO.readFile("src/ba/bitcamp/hajrudin/mini/game/gameFile.txt");
-			String s = "";
-			while(!TextIO.eof()){
-				s+=TextIO.getln();
-				s+=",";
-			}
-			String ar[] = scoreLabel.getText().split(" ");
-			int a = Integer.parseInt(ar[1]);
-			s+=nameText.getText()+" "+a;
-			String []arr = s.split(",");
-			TextIO.writeFile("src/ba/bitcamp/hajrudin/mini/game/gameFile.txt");
-			for(int i = 0; i<arr.length; i++){
-				TextIO.putln(arr[i]);
-			}
+		String s = "";
+		while (!TextIO.eof()) {
+			s += TextIO.getln();
+			s += ",";
+		}
+		String ar[] = scoreLabel.getText().split(" ");
+		int a = Integer.parseInt(ar[1]);
+		s += nameText.getText() + " " + a;
+		String[] arr = s.split(",");
+		TextIO.writeFile("src/ba/bitcamp/hajrudin/mini/game/gameFile.txt");
+		for (int i = 0; i < arr.length; i++) {
+			TextIO.putln(arr[i]);
+		}
 	}
-	
-	private class StartButtonAction implements ActionListener{
+
+	private class StartButtonAction implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			gamePanel.setVisible(true);
 		}
-		
+
 	}
-	
-	private class ShowScoresButton implements ActionListener{
+
+	private class ShowScoresButton implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			/**TextIO.readFile("Resources/gameFile.txt");
+			TextIO.readFile("src/ba/bitcamp/hajrudin/mini/game/gameFile.txt");
 			String s = "";
-			while(!TextIO.eof()){
-				s+=TextIO.getln();
-				s+="\n";
-			}**/	
-			
-			  String fileName = "src/ba/bitcamp/hajrudin/mini/game/gameFile.txt";
+			while (!TextIO.eof()) {
+				s += TextIO.getln();
+				s += "\n";
+			}
 
-		        // This will reference one line at a time
-		        String line = null;
-		        String s = "";
-
-		        try {
-		            // FileReader reads text files in the default encoding.
-		            FileReader fileReader = 
-		                new FileReader(fileName);
-
-		            // Always wrap FileReader in BufferedReader.
-		            BufferedReader bufferedReader = 
-		                new BufferedReader(fileReader);
-
-		            while((line = bufferedReader.readLine()) != null) {
-		                System.out.println(line);
-		                s+=line;
-		            }    
-
-		            // Always close files.
-		            bufferedReader.close();            
-		        }
-		        catch(FileNotFoundException ex) {
-		            System.out.println(
-		                "Unable to open file '" + 
-		                fileName + "'");                
-		        }
-		        catch(IOException ex) {
-		            System.out.println(
-		                "Error reading file '" 
-		                + fileName + "'");                   
-		            // Or we could just do this: 
-		            // ex.printStackTrace();
-		        }
-		    
-			
 			JOptionPane.showMessageDialog(null, s);
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
